f82d63753f0f6ba0fbe6063e0bb9ec42de91a3e7
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-11-02 19:32:18 +0100

    Initial commit

---
+++ 04-strings-concatenation.js
@@ -0,0 +1,24 @@
+// node 04-strings-concatenation.js
+
+// ЗАДАЧА - Объединение строк
+//
+// 1. Объявите три переменных с значениями:
+// - ваше имя
+// - ваша фамилия
+// - ваша профессия
+//
+// 2. Создайте еще одну переменную. Ее значение должно быть ,например
+// "Меня зовут <имя> <фамилия> и я <Профессия>"
+//
+// 3. Выведите значение переменной в консоль
+
+let firstName = 'Yulia'
+let lastName = 'Slaposka'
+let profession = 'Chief'
+
+// let hi = `Меня зовут ${firstName} ${lastName} и я ${profession}`
+ let hi = 'Меня зовут ' + firstName + ' ' + lastName + ' и я ' + profession
+
+
+console.log(hi)
+
