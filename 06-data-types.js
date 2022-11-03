ccc53e92307895378bf105c6bf4eb5d9b7284c6b
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-11-03 18:59:45 +0100

    add 06-08

---
+++ 06-data-types.js
@@ -0,0 +1,39 @@
+// node 06-data-types.js
+
+/** ЗАДАЧА - Типы данных
+ *
+ * 1. Обьявите несколько переменных и присвойте им значения:
+ * - строка
+ * - число
+ * - логическое
+ * - null
+ * - undefined
+ * - объект
+ * - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений используя оператор typeOf
+ */
+
+let string = 'stringa'
+console.log(typeof string)
+
+let number = 847
+console.log(typeof number)
+
+let boolean = true
+console.log(typeof boolean)
+
+let nulL = null
+console.log(typeof nulL)
+//создает пустое значение для объекта . это объект
+
+let undefineD = undefined
+console.log(typeof undefineD)
+
+let object = {type: 'houses'}
+console.log(typeof object)
+
+let array = [89, 78, 'jimi', 434, 'jea']
+console.log(typeof array)
+// нет отдельного типа для массива. это объект .
+
